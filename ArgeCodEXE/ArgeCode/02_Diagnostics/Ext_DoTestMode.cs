4d2fa74c3bfc1cbc0a75f7cfefb439c018993389
qwe <qwe>
2021-11-14 15:50:39 +0300

    ***

---
+++ ArgeCodEXE/ArgeCode/02_Diagnostics/Ext_DoTestMode.cs
@@ -0,0 +1,81 @@
+﻿using Component.ConsoleR.ReadER;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Component.ConsoleR;
+///////////////////////////////////////////////////////////
+namespace Component.Diagnostics
+{
+    //Component.Att_TestLast//Component.TestLast.Do()//
+    [AttributeUsage(AttributeTargets.Method)]
+    public class Att_TestLast : System.Attribute
+    {
+        public System.DateTime p_DateTime = new DateTime();
+        public Att_TestLast() { }
+        public Att_TestLast(int _year=0, int _month=0, int _day=0, int _hour=0, int _minute=0, int _second=0, int _millisecond=0)
+        {
+            this.p_DateTime = new DateTime(_year, _month, _day, _hour, _minute, _second, _millisecond);
+        }
+        public override string ToString()
+        {
+            return this.GetType().FullName.ToString()+"[" + this.p_DateTime.ToString()+"]";
+        }
+    }
+    /// <summary>
+    /// Продвинутый инструмент запуска тестовых функций.
+    /// Как пользоваться. Создаете статическую тестовую вункцию.
+    /// Помечаете ее атрибутом тестовых функций:
+    /// [Component.Att_TestMode(_year: 2020, _month: 07, _day: 27, _hour: 15, _minute: 43, _second: 0, _millisecond: 0)]
+    /// Выставляете параметры глядя на карманные часы)))
+    /// В самом коде втыкаете Component.TestMode.On();
+    /// ///////////////////////////////////////////////////////////
+    /// Как это работает: рефлексивный LinQ-шный поиск по типам классов с поиском самого позднего System.DateTime , сидящего в атрибуте
+    /// </summary>
+    public static class TestLast
+    {
+        public static void Do()
+        {
+            AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(_Assemblie => _Assemblie.GetTypes().ToArray())
+                .SelectMany(_Type => _Type.GetMethods().Where(a => a.IsStatic).Select(a => new { p_Method =a, p_Type = _Type}))
+                .Where(a => a.p_Method.GetCustomAttributes(true).OfType<Att_TestLast>().Count() == 1)
+                .SetIf(_fBool: a => a.Count() == 0, _f1: a => throw new System.Exception("Eror:20200727_1004:Ошибка запуска тест сценария. Нет тестовых функций помеченных атрибутом <" + typeof(Att_TestLast).ToString() + ">"))
+                .Get(a=> {
+                    System.DateTime _DateTime =
+                        a.Select(b => b.p_Method.GetCustomAttributes(true).OfType<Att_TestLast>().First().p_DateTime).Max();
+                    ;
+                    return a.Where(b => b.p_Method.GetCustomAttributes(true).OfType<Att_TestLast>().First().p_DateTime == _DateTime);
+                })
+                .First()
+                .Set(a => Component.StaticC.p_IConsoleR
+                    .WriteLine(a.p_Method.GetCustomAttributes(true).OfType<Att_TestLast>().First().ToString())
+                    .Set_(b =>b.Set_Test().WriteLine(a.p_Type.FullName + "." + a.p_Method.Name + "()"))
+                ).p_Method
+                .CreateDelegate(typeof(System.Action))
+                .Cast_As<System.Action>()()
+            ;
+        }
+        //private struct Q
+        //Пример того, как тестовые функции запускались ранее
+        /// <summary>Component.TestMode.Test();</summary>
+        public static void Test()
+        {
+            Component.StaticC.CheckStackTracer();
+            Component.Diagnostics.TestLast.Do();
+        }
+        //Пример того, как тестовые функции можно мапить сейчас
+        [Component.Diagnostics.Att_TestLast(_year:2020,_month:07,_day:27,_hour:15,_minute:40,_second:0,_millisecond:0)]
+        public static void Test_0()
+        {
+            Component.StaticC.CheckStackTracer();
+        }
+        [Component.Diagnostics.Att_TestLast(_year: 2020, _month: 07, _day: 27, _hour: 15, _minute: 43, _second: 0, _millisecond: 0)]
+        public static void Test_1()
+        {
+            Component.StaticC.CheckStackTracer();
+        }
+    }
+}
---
+++ ArgeCodEXE/ArgeCode/02_Diagnostics/StackTracer.cs
@@ -0,0 +1,46 @@
+﻿namespace Component.Diagnostics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    ////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>Анализатор стека вызова функций</summary>
+    public class StackTracer
+    {
+        public int p_StepBackShift = -1;
+        public int p_BaseShift = 4;
+        public IList<string> p_ILS = new List<string>();
+        public string p_str_StackTrace = "";
+        public StackTracer()
+        {
+            this.p_str_StackTrace = Environment.StackTrace;
+            this.p_ILS = this.p_str_StackTrace.Split('\n').ToList<string>();
+        }
+        public StackTracer Set(System.Nullable<int> _StepBackShift = null, System.Nullable<int> _BaseShift = null)
+        {
+            if (_StepBackShift != null) this.p_StepBackShift = _StepBackShift.Value;
+            if (_BaseShift != null) this.p_BaseShift = _BaseShift.Value;
+            return this;
+        }
+        public string Get_STSS()
+        {
+            ;
+            int _Shift = this.p_BaseShift + this.p_StepBackShift;
+            if (this.p_ILS.Count > _Shift) return this.p_ILS[_Shift].Split('в')[1];
+            throw new System.Exception("Component.StackTracer.Get_STSS() /n Eror");
+        }
+        public static implicit operator System.String(StackTracer _StackTracer) { return _StackTracer.Get_STSS(); }
+        public StackTracer writeThis() { this.p_ILS.ToList().ForEach(a => System.Console.WriteLine(a)); return this; }
+        /// <summary>
+        /// Консольный тест.
+        /// Консольные тесты пишутся так что при запуске модульных тестов можно перенаправить вывод в лог файлы
+        /// </summary>
+        public static void Test()
+        {
+            System.Console.WriteLine("Component.StackTracer Class");
+            StackTracer _IStackTracer = new StackTracer();
+            _IStackTracer.writeThis();
+            System.Console.WriteLine(_IStackTracer.Get_STSS());
+        }
+    }
+}
