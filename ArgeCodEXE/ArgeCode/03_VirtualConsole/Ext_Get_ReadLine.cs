dab183fb4237152a87b9ecc0eb0ff4118a33cd37
qwe <qwe>
2021-11-23 08:18:08 +0300

    *********

--- ArgeCodEXE/ArgeCode/03_VirtualConsole/Ext_Get_ReadLine.cs
+++ ArgeCodEXE/ArgeCode/03_VirtualConsole/Ext_Get_ReadLine.cs
@@ -32,6 +32,8 @@ namespace System.VConsole
             }
             return _this;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 05, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Boolean(){System.Diagnostics.StackTracer.Get_STS().WriteLine();(new System.Boolean()).Get_ReadLine().ToString().WriteLine();}
         public static System.Byte Get_ReadLine(this System.Byte _this)
         {
             System.Byte rez = 0;
@@ -40,6 +42,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 07, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Byte() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Byte()).Get_ReadLine().ToString().WriteLine(); }
         public static System.Char Get_ReadLine(this System.Char _this)
         {
             System.Char rez = ' ';
@@ -48,6 +52,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 08, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Char() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Char()).Get_ReadLine().ToString().WriteLine(); }
         public static System.Decimal Get_ReadLine(this System.Decimal _this)
         {
             System.Decimal rez = 0;
@@ -56,6 +62,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 09, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Decimal() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Decimal()).Get_ReadLine().ToString().WriteLine(); }
         public static System.Double Get_ReadLine(this System.Double _this)
         {
             System.Double rez = 0;
@@ -64,6 +72,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 10, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Double() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Double()).Get_ReadLine().ToString().WriteLine(); }
         public static System.Int16 Get_ReadLine(this System.Int16 _this)
         {
             System.Int16 rez = 0;
@@ -72,6 +82,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 11, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Int16() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Int16()).Get_ReadLine().ToString().WriteLine(); }
         public static System.Int32 Get_ReadLine(this System.Int32 _this)
         {
             System.Int32 rez = 0;
@@ -80,6 +92,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 12, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Int32() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Int32()).Get_ReadLine().ToString().WriteLine(); }
         public static System.Int64 Get_ReadLine(this System.Int64 _this)
         {
             System.Int64 rez = 0;
@@ -88,6 +102,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 13, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Int64() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Int64()).Get_ReadLine().ToString().WriteLine(); }
         public static System.SByte Get_ReadLine(this System.SByte _this)
         {
             System.SByte rez = 0;
@@ -96,6 +112,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 14, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_SByte() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.SByte()).Get_ReadLine().ToString().WriteLine(); }
         public static System.Single Get_ReadLine(this System.Single _this)
         {
             System.Single rez = 0;
@@ -104,7 +122,11 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 15, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_Single() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.Single()).Get_ReadLine().ToString().WriteLine(); }
         public static System.String Get_ReadLine(this System.String _this) { return System.VConsole.Config.p_VirtualConsole.ReadLine(); }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 16, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_String() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); "".Get_ReadLine().ToString().WriteLine(); }
         public static System.UInt16 Get_ReadLine(this System.UInt16 _this)
         {
             System.UInt16 rez = 0;
@@ -113,6 +135,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 16, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_UInt16() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.UInt16()).Get_ReadLine().ToString().WriteLine(); }
         public static System.UInt32 Get_ReadLine(this System.UInt32 _this)
         {
             System.UInt32 rez = 0;
@@ -121,6 +145,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 17, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_UInt32() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.UInt32()).Get_ReadLine().ToString().WriteLine(); }
         public static System.UInt64 Get_ReadLine(this System.UInt64 _this)
         {
             System.UInt64 rez = 0;
@@ -129,5 +155,8 @@ namespace System.VConsole
             ) ;
             return rez;
         }
+        [System.Diagnostics.Att_TestLast(_year: 2021, _month: 11, _day: 23, _hour: 8, _minute: 18, _second: 0, _millisecond: 0)]
+        public static void Test_Get_ReadLine_System_UInt64() { System.Diagnostics.StackTracer.Get_STS().WriteLine(); (new System.UInt64()).Get_ReadLine().ToString().WriteLine(); }
+
     }
 }
