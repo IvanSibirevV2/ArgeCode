1aa0b6237035efc96eb2003f492ce3c3f5f56257
qwe <qwe>
2023-02-09 10:07:38 +0300

    ********

--- ArgeCodEXE/ArgeCode/01_System/Diagnostics/TestLast.cs
+++ ArgeCodEXE/ArgeCode/01_System/Diagnostics/TestLast.cs
@@ -16,11 +16,11 @@ namespace System.Diagnostics
     public static class Ext_ListTestLastMethod
     {
         public static TestLastMethod Get_LastTestMetod(this List<TestLastMethod> _TestLastMethodS)
-        {return _TestLastMethodS.Select(b => b.p_DateTime).Max().Get(_Max => _TestLastMethodS.Where(b => b.p_DateTime == _Max)).First(); }
+        { return _TestLastMethodS.Select(b => b.p_DateTime).Max().Get(_Max => _TestLastMethodS.Where(b => b.p_DateTime == _Max)).First(); }
         public static List<TestLastMethod> Where_BoolTest(this List<TestLastMethod> _List_TestLastMethod)
-        {return _List_TestLastMethod.Where(a => a.p__Target_MethodInfo.ReturnParameter.ParameterType == typeof(System.Boolean)).ToList(); }
+        { return _List_TestLastMethod.Where(a => a.p__Target_MethodInfo.ReturnParameter.ParameterType == typeof(System.Boolean)).ToList(); }
         public static List<TestLastMethod> Where_VoidTest(this List<TestLastMethod> _List_TestLastMethod)
-        {return _List_TestLastMethod.Where(a => a.p__Target_MethodInfo.ReturnParameter.ParameterType == typeof(void)).ToList();}
+        { return _List_TestLastMethod.Where(a => a.p__Target_MethodInfo.ReturnParameter.ParameterType == typeof(void)).ToList(); }
     }
     /// <summary>
     /// Продвинутый инструмент запуска тестовых функций.
@@ -52,18 +52,18 @@ namespace System.Diagnostics
             return TestS;
             //return null;
         }
-        
+
         /// <summary> Запуск последнего тестового метода, возвращающего System.Boolean </summary>
         public static System.Boolean Do_IfBoolean()
-        {return System.Diagnostics.TestLast.Get_TestS().Where_BoolTest().Get_LastTestMetod().WriteThis().Get_Delegate_AS<System.Func<System.Boolean>>()();}
+        { return System.Diagnostics.TestLast.Get_TestS().Where_BoolTest().Get_LastTestMetod().WriteThis().Get_Delegate_AS<System.Func<System.Boolean>>()(); }
         /// <summary> Запуск вообще всех тестов bool. Предупреждение тест bool не может использовать пользовательский ввод и вывод</summary>
         public static System.Boolean Do_IfBoolean_Oll()
-        {return System.Diagnostics.TestLast.Get_TestS().Where_BoolTest().Select(a => a.Get_Delegate_AS<System.Func<System.Boolean>>()()).Aggregate((a, b) => a & b);}
+        { return System.Diagnostics.TestLast.Get_TestS().Where_BoolTest().Select(a => a.Get_Delegate_AS<System.Func<System.Boolean>>()()).Aggregate((a, b) => a & b); }
         /// <summary> Запуск последнего тестового метода, возвращающего System.Boolean </summary>
-        public static void Do_IfVoid(){System.Diagnostics.TestLast.Get_TestS().Where_VoidTest().Get_LastTestMetod().WriteThis().Get_Delegate_AS<System.Action>()();}
+        public static void Do_IfVoid() { System.Diagnostics.TestLast.Get_TestS().Where_VoidTest().Get_LastTestMetod().WriteThis().Get_Delegate_AS<System.Action>()(); }
         /// <summary> Запуск вообще всех тестов void. Предупреждение тест void может использовать пользовательский ввод и вывод</summary>
         public static void Do_IfVoid_Oll()
-        {System.Diagnostics.TestLast.Get_TestS().Where_VoidTest().ForEach(a => a.Get_Delegate_AS<System.Action>()());}
+        { System.Diagnostics.TestLast.Get_TestS().Where_VoidTest().ForEach(a => a.Get_Delegate_AS<System.Action>()()); }
         /// <summary> Запуск последнего тестового метода, возвращающего System.Boolean </summary>
         public static System.Boolean Do()
         { return System.Diagnostics.TestLast.Get_TestS().Where_BoolTest().Get_LastTestMetod().WriteThis().Get_Delegate_AS<System.Func<System.Boolean>>()(); }
@@ -81,12 +81,12 @@ namespace System.Diagnostics
         //Пример того, как тестовые функции можно мапить сейчас
         [System.Diagnostics.TestLastMethod(_year: 2023, _month: 2, _day: 2, _hour: 18, _minute: 23, _second: 0, _millisecond: 0, _StrComment: "Тест №0. Мапинг тестовых функций ")]
         public static bool Test_0()
-        {System.Console.WriteLine(System.Diagnostics.StackTracer.Get_STS()); return true; }
+        { System.Console.WriteLine(System.Diagnostics.StackTracer.Get_STS()); return true; }
         [System.Diagnostics.TestLastMethod(_year: 2023, _month: 2, _day: 2, _hour: 18, _minute: 23, _second: 1, _millisecond: 0, _StrComment: "Тест №0. Мапинг тестовых функций ")]
         public static System.Boolean Test_0XT78587GF874444G()
-        {System.Console.WriteLine(System.Diagnostics.StackTracer.Get_STS());return true;}
+        { System.Console.WriteLine(System.Diagnostics.StackTracer.Get_STS()); return true; }
         [System.Diagnostics.TestLastMethod(_year: 2021, _month: 11, _day: 17, _hour: 15, _minute: 06, _second: 0, _millisecond: 0, _StrComment: "Тест №2. Мапинг тестовых функций ")]
         public static bool Test_1()
-        {System.Console.WriteLine(System.Diagnostics.StackTracer.Get_STS());return true;}
+        { System.Console.WriteLine(System.Diagnostics.StackTracer.Get_STS()); return true; }
     }
 }
--- ArgeCodEXE/ArgeCode_NetCore3/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ArgeCode_NetCore3
-{
-    public class Class1
-    {
-    }
-}
