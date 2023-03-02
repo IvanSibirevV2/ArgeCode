b4fb22d323b58611c56d6aee1ca3e895d92b89b4
qwe <qwe>
2023-03-02 11:15:32 +0300

    +selected list
    ArgeCode.0.0.7

---
+++ ArgeCodEXE/ArgeCode/02_System_T/SelectedList_T.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public class SelectedList<T>
+    {
+        public List<T> p_ListT= new List<T>();
+        private int p__SelectedIndex = -1;
+        public int p_SelectedIndex
+        {
+            get {return this.ValidatSelectedIndex().p__SelectedIndex;}
+            set {this.p__SelectedIndex = value;this.ValidatSelectedIndex();}
+        }
+        public SelectedList<T> ValidatSelectedIndex()
+        {
+            if (this.p__SelectedIndex > this.p_ListT.Count()) this.p__SelectedIndex = this.p_ListT.Count() - 1;
+            if (this.p__SelectedIndex < -1) this.p__SelectedIndex = -1;
+            return this;
+        }
+        public T p_SelectedItem { 
+            get{ return this.p_ListT[this.p_SelectedIndex];}
+            set { this.p_ListT[this.p_SelectedIndex] = value; }
+        }
+    }
+}
